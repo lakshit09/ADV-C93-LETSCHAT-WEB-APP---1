54ae117b2f42ccd29323ae25d1036f8d32842da0
lakshit09 <92573746+lakshit09@users.noreply.github.com>
2022-01-30 10:12:40 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
